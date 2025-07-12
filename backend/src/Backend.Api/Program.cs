18c01a4645cd9b65c9a3a066c68cb15e7d4a225c
Francisco Sierra Luciarte <62906570+franciscofsl@users.noreply.github.com>
2025-07-12 08:34:08 +0200

    add cors in server

--- backend/src/Backend.Api/Program.cs
+++ backend/src/Backend.Api/Program.cs
@@ -6,6 +6,18 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+// Add CORS services
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowReactApp", policy =>
+    {
+        policy.WithOrigins("http://localhost:5173")
+              .AllowAnyHeader()
+              .AllowAnyMethod()
+              .AllowCredentials();
+    });
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -13,6 +25,9 @@ if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
 }
+
+// Use CORS policy
+app.UseCors("AllowReactApp");
 
 app.UseHttpsRedirection();
 
